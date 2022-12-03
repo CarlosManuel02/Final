872120944880db64a36b01d8758571dcfcb39cee
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2022-12-02 22:48:39 -0700

    Initial commit

---
+++ src/final/java/Main/MyConnection.java
@@ -0,0 +1,55 @@
+package Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Carlos
+ */
+public class MyConnection {
+
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/final", "root", "noway123");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return con;
+    }
+
+    public static Connection getConection() {
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/alma", "root", "noway123");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return con;
+    }
+
+    public Connection getconnection() {
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/final", "root", "noway123");
+        } catch (Exception ex) {
+            System.out.println("Error en la conexion" + ex);
+        }
+        return con;
+    }
+
+}   
+
+
+    
+    
+
